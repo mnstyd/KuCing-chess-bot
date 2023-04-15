0dc5fc325d37b54617ca4693664e9ff8248d3cb5
Iman Satyo Adi <85503165+mnstyd@users.noreply.github.com>
2023-04-16 05:22:03 +0700

    Add files via upload
    more features :D

--- kucing.py
+++ kucing.py
@@ -1,33 +1,37 @@
 import chess.engine
 import random
+import time
 
 class KuCing:
-    def __init__(self):
+    def __init__(self, difficulty=1):
         self.board = chess.Board()
+        self.difficulty = difficulty
         self.engine = chess.engine.SimpleEngine.popen_uci("stockfish-windows-2022-x86-64-avx2")
-
-    def make_move(self, move_string):
-        try:
-            move = chess.Move.from_uci(move_string)
-            if move not in self.board.legal_moves:
-                print("Invalid move. Please make a legal move.")
-                return False
-            else:
-                self.board.push(move)
-                return True
-        except ValueError:
-            print("Invalid move format. Please enter your move in the format 'e2e4'.")
-            return False
+        self.color = chess.WHITE
+        self.is_player_turn = True
 
     def play_random_move(self):
         legal_moves = list(self.board.legal_moves)
         move = random.choice(legal_moves)
         self.board.push(move)
         return move
+        
+
+    def make_move(self, player_move):
+        move = chess.Move.from_uci(player_move)
+        while move not in self.board.legal_moves:
+            print("Invalid move, please try again.")
+            move = input("Enter your move in UCI notation (e.g. e2e4): ")
+            move = chess.Move.from_uci(move)
+        self.board.push(move)
+        return True
 
     def play_engine_move(self):
-        result = self.engine.play(self.board, chess.engine.Limit(time=2.0))
+        self.is_player_turn = False
+        # print("KuCing is thinking...")
+        result = self.engine.play(self.board, chess.engine.Limit(time=self.difficulty))
         self.board.push(result.move)
+        self.is_player_turn = True
         return result.move
 
     def is_game_over(self):
@@ -35,6 +39,31 @@ class KuCing:
 
     def get_board(self):
         return str(self.board)
+
+    def get_difficulty(self):
+        return self.difficulty
+
+    def change_difficulty(self, new_difficulty):
+        self.difficulty = new_difficulty
+        print(f"Difficulty level changed to {self.difficulty}.")
+
+    def get_color(self):
+        return self.color
+
+    def switch_color(self):
+        self.color = not self.color
+
+    def get_player_turn(self):
+        return self.is_player_turn
+
+    def get_game_result(self):
+        result = self.board.result()
+        if result == "1-0":
+            print("Congratulations, you won!")
+        elif result == "0-1":
+            print("KuCing won! Don't give up, try again!")
+        else:
+            print("Good game, GG!")
 
     def __del__(self):
         self.engine.quit()
--- play_kucing.py
+++ play_kucing.py
@@ -1,83 +1,73 @@
-import chess.engine
-import random
+import os
+import sys
 import time
 
-class KuCing:
-    def __init__(self, difficulty):
-        self.board = chess.Board()
-        self.engine = chess.engine.SimpleEngine.popen_uci("stockfish-windows-2022-x86-64-avx2")
-        self.difficulty = difficulty
-        print("Hi, I'm KuCing! Let's play some chess.")
+from kucing import KuCing
+import chess
 
-    def make_move(self, move):
-        if move not in self.board.legal_moves:
-            print("Invalid move. Please make a legal move.")
-            return False
-        else:
-            self.board.push(move)
-            return True
-
-    def play_random_move(self):
-        legal_moves = list(self.board.legal_moves)
-        move = random.choice(legal_moves)
-        self.board.push(move)
-        return move
-
-    def play_engine_move(self):
-        result = self.engine.play(self.board, chess.engine.Limit(depth=self.difficulty))
-        self.board.push(result.move)
-        return result.move
-
-    def is_game_over(self):
-        return self.board.is_game_over()
-
-    def get_board(self):
-        return str(self.board)
-
-    def __del__(self):
-        self.engine.quit()
+def main():
+    print("Hi, I'm KuCing! Let's play some chess.")
+    while True:
+        try:
+            difficulty = int(input("Please choose the difficulty level (1-10): "))
+            if difficulty < 1 or difficulty > 10:
+                print("Please enter a number between 1 and 10.")
+            else:
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 10.")
+    
+    board = chess.Board()
+    kucing = KuCing(difficulty)
+    time.sleep(1)
+    print("\nHello! I'm KuCing and I'll be playing against you in this game of chess.")
+    time.sleep(1)
+    print(f"KuCing's difficulty level is set to {difficulty}. Good luck and have fun!")
+    time.sleep(1)
+    print("KuCing plays as black. You play as white. Let's start!\n")
+    time.sleep(1)
+    
+    while not board.is_game_over():
+        print("Current board:\n", board)
+    
+        while True:
+            player_move = input("Enter your move in UCI notation (e.g. e2e4): ")
+        
+            if player_move.lower() == "resign":
+                print("You resigned! Better luck next time!")
+                sys.exit(0)
+            
+            try:
+                move = chess.Move.from_uci(player_move)
+                if move in board.legal_moves:
+                    break
+                else:
+                    print("Invalid move. Please enter a valid move.")
+            except ValueError:
+                print("Invalid move. Please enter a valid move. (e.g. e2e4):")
+    
+        board.push(move)
+    
+        print(f"\nKuCing is thinking...")
+        kucing_move = kucing.play_engine_move()
+        while kucing_move not in board.legal_moves:
+            kucing_move = kucing.play_engine_move()
+        print(f"KuCing played: {kucing_move}\n")
+        board.push(kucing_move)
+    
+        if board.is_game_over():
+            result = board.result()
+            print("\nFinal board:\n", board)
+            if result == "1-0":
+                print("Congratulations! You won the game!")
+                kucing.congratulate()
+            elif result == "0-1":
+                print("Sorry, you lost the game.")
+                kucing.motivate()
+            else:
+                print("The game ended in a draw. Good game!")
+                kucing.good_game()
+            break
 
 if __name__ == '__main__':
-    def main():
-        while True:
-            difficulty = input("Please choose the difficulty level (1-10): ")
-            if difficulty.isdigit() and 1 <= int(difficulty) <= 10:
-                break
-            else:
-                print("Invalid input. Difficulty level must be between 1-10. Please try again.")
-        kucing = KuCing(int(difficulty))
-
-        print("The game has started!")
-
-        print(kucing.get_board())
-        while not kucing.is_game_over():
-            user_move = input("Enter your move (e.g. e2e4): ")
-            user_move = chess.Move.from_uci(user_move)
-
-            print("KuCing is thinking...")
-            time.sleep(3)
-
-            success = kucing.make_move(user_move)
-            print(f"KuCing played: {success}")
-            if not success:
-                continue
-
-            print(kucing.get_board())
-
-            if kucing.is_game_over():
-                if kucing.board.is_checkmate():
-                    print("Congratulations, you won!")
-                    kucing.__del__()
-                elif kucing.board.is_stalemate() or kucing.board.is_insufficient_material():
-                    print("Good game! It's a draw!")
-                    kucing.__del__()
-                else:
-                    print("Sorry, you lost. Don't give up!")
-                    kucing.__del__()
-
-            else:
-                kucing_move = kucing.play_engine_move()
-                print(f"KuCing plays {kucing_move}")
-                print(kucing.get_board())
-
     main()
